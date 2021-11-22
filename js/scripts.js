39d108bf9cebfd5e60701b175ca69d7ef723ef2b
Oladeji Daniel <oladejidaniel456@gmail.com>
2021-11-22 02:02:56 -0800

    Create the BLI for not a triangle

--- js/scripts.js
+++ js/scripts.js
@@ -20,3 +20,10 @@ function scalene(side1 , side2 , side3){
         return false;
     }
 } 
+function notTriangle(side1 , side2 , side3){
+    if(side1 < side2 + side3 || side2 < side1 +side3 || side3 < side1 + side2){
+        return true
+    }else{
+        return false;
+    }
+}
